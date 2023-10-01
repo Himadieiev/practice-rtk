17b542a94dd3061c14ac0ff655f16af223894acf
Ruslan Himadieiev <113462988+Himadieiev@users.noreply.github.com>
2023-10-01 17:58:55 +0300

    added todoSlice

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,11 @@ import Form from "./components/Form";
 import User from "./components/User";
 import TodoItem from "./components/TodoItem";
 import Posts from "./components/Posts";
+import { useSelector } from "react-redux";
 
 function App() {
+  const todos = useSelector((state) => state.todo.todos);
+
   return (
     <div className="min-h-screen h-full w-screen bg-indigo-400">
       <div className="container mx-auto px-4">
@@ -17,7 +20,9 @@ function App() {
           <div className="w-1/3">
             <h1 className="font-bold my-5">Redux Toolkit Todo App</h1>
             <Form />
-            <TodoItem />
+            {todos?.map((todo) => (
+              <TodoItem key={todo.id} todo={todo} />
+            ))}
           </div>
           <div className="w-1/3">
             <h1 className="font-bold my-5">Redux Toolkit Async Thunk</h1>
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,21 +1,37 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { removeTodo, toggleCompletedTodo } from "../features/todo/todoSlice";
 
-const TodoItem = () => {
-  const completed = false;
+const TodoItem = ({ todo }) => {
+  const dispatch = useDispatch();
+
+  const toggleTodoHandler = (id) => {
+    dispatch(toggleCompletedTodo(id));
+  };
+
+  const removeTodoHandler = (id) => {
+    dispatch(removeTodo(id));
+  };
 
   return (
     <div className="flex justify-between items-center my-2">
-      <div className="text-sm px-4 py-2 cursor-pointer bg-lime-300 hover:bg-lime-400">
+      <div
+        className="text-sm px-4 py-2 cursor-pointer bg-lime-300 hover:bg-lime-400"
+        onClick={() => toggleTodoHandler(todo.id)}
+      >
         Complete
       </div>
       <div
         className={`text-sm ${
-          completed ? "line-through font-medium text-lime-400" : ""
+          todo.completed ? "line-through font-medium text-lime-400" : ""
         }`}
       >
-        Todo Text
+        {todo.text}
       </div>
-      <div className="text-sm px-4 py-2 flex bg-red-400 hover:bg-red-500 transition-all text-white cursor-pointer">
+      <div
+        className="text-sm px-4 py-2 flex bg-red-400 hover:bg-red-500 transition-all text-white cursor-pointer"
+        onClick={() => removeTodoHandler(todo.id)}
+      >
         Delete
       </div>
     </div>
