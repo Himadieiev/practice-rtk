fae94bd42b0ab9fe0c539563f6645d5b7fdb8d38
Ruslan Himadieiev <113462988+Himadieiev@users.noreply.github.com>
2023-10-01 15:55:02 +0300

    added App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="min-h-screen h-full w-screen bg-indigo-400">
+      <div className="container mx-auto px-4">
+        <header className="flex gap-20 ">
+          <div className="w-1/3">
+            <h1 className="font-bold my-5">Redux Toolkit State Change</h1>
+          </div>
+          <div className="w-1/3">
+            <h1 className="font-bold my-5">Redux Toolkit Todo App</h1>
+          </div>
+          <div className="w-1/3">
+            <h1 className="font-bold my-5">Redux Toolkit Async Thunk</h1>
+          </div>
+        </header>
+      </div>
+    </div>
+  );
 }
 
 export default App;
